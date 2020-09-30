147b235eaca653c1f7813dd2cc27aaf6a1bb6b95
jeffrysteven <jeffrysteven@users.noreply.github.com>
2020-09-30 15:25:08 +0000

    deploy: 4eccc2c829ffcbf0bfdb32638effbcd314df72d8

---
+++ precache-manifest.547fe4e447f55ff3ab03b6d3b4e1bacc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa47e59c8cc91ff5e3235073cb86c68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d19d7f4c19d5de4fa668",
+    "url": "/static/css/2.d48510fc.chunk.css"
+  },
+  {
+    "revision": "e59761535812b8bbec2f",
+    "url": "/static/css/main.120037b6.chunk.css"
+  },
+  {
+    "revision": "d19d7f4c19d5de4fa668",
+    "url": "/static/js/2.59f933fb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.59f933fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59761535812b8bbec2f",
+    "url": "/static/js/main.56442341.chunk.js"
+  },
+  {
+    "revision": "b09a0e02381650fc2a01",
+    "url": "/static/js/runtime-main.473267d5.js"
+  }
+]);
